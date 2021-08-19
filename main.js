4a404789c013cee24990a5b520ce45c8c37893a0
Eduardo Guzman Lau Len <eduardo.laulen@nzxt.com>
2021-08-19 17:52:18 -0400

    chore: add example for contextIsolation

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
     },
   });
 
